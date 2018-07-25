7b0a2544c54098f308b887e22278d15a273c37ad
Lisa Steele <lisahsteele@gmail.com>
2018-07-25 15:17:55 -0700

    Created: HomeModel file to handle json data retrieved from DB. UserModel file to hold class of users.

---
+++ MenuPlanner/HomeModel.swift
@@ -0,0 +1,13 @@
+//
+//  HomeModel.swift
+//  MenuPlanner
+//
+//  Created by Lisa Steele on 7/25/18.
+//  Copyright © 2018 lisahsteele. All rights reserved.
+//
+
+import UIKit
+
+class HomeModel: NSObject {
+
+}
---
+++ MenuPlanner/UserModel.swift
@@ -0,0 +1,13 @@
+//
+//  UserModel.swift
+//  MenuPlanner
+//
+//  Created by Lisa Steele on 7/25/18.
+//  Copyright © 2018 lisahsteele. All rights reserved.
+//
+
+import UIKit
+
+class UserModel: NSObject {
+
+}
